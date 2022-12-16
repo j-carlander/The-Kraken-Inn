4b932a26c6e2617d1789b2ad0ca6a7b5d277681e
Jonathan Andersson <2jonand@gmail.com>
2022-12-16 15:38:31 +0100

    added some class names in html-files, added structure for floating nav-bar to all html's, added functions for scrolling to category in menu

---
+++ src/app.js
@@ -0,0 +1,46 @@
+const beverageSection = document.querySelector(".beverage-section");
+const appetizerSection = document.querySelector(".appetizer-section");
+const mainCourseSection = document.querySelector(".main-course-section");
+const dessertSection = document.querySelector(".dessert-section");
+const menuCategory = document.querySelectorAll(".menu-category");
+
+function scrollToBeverage() {
+  beverageSection.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+function scrollToAppetizers() {
+  appetizerSection.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+function scrollToMainCourse() {
+  mainCourseSection.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+function scrollToDesserts() {
+  dessertSection.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    inline: "nearest",
+  });
+}
+
+// for (category of menuCategory) {
+//   category.addEventListener("click", scrollToCategory);
+// }
+
+// function scrollToCategory() {
+//   for (let i = 0; i < menuCategory.length; i++) {
+//     if (category.classList.contains("beverage")) {
+//       console.log("beverage");
+//     }
+//   }
+// }
