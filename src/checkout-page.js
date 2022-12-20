12408ec4e74e6a504736abae764529351515d15b
j-carlander <johannes.carlander@gmail.com>
2022-12-20 11:05:29 +0100

    bröt ner funktionen addToCart till flera mindre funktioner.

--- src/checkout-page.js
+++ src/checkout-page.js
@@ -1,12 +1,9 @@
-
-
-
-let shoppingList = [] 
+let shoppingList = [];
 
 // checkout-page
 let shoppingCart = document.querySelector(".shopping-cart");
 // let addToCheckoutBtn = document.querySelector(".add-qty-btn");
-let plusBtn = document.querySelector('.cart-plus-btn')
+let plusBtn = document.querySelector(".cart-plus-btn");
 
 // addToCheckoutBtn.addEventListener('click', addToCheckoutClicked)
 // function addToCheckoutClicked(event){
@@ -20,44 +17,75 @@ let plusBtn = document.querySelector('.cart-plus-btn')
 // }
 // function allProducts(foodName, foodPrice, foodimgSrc){
 //     // let checkout = foodName.foodPrice;
-//     // let item = checkout.foodimgSrc; 
+//     // let item = checkout.foodimgSrc;
 //     // item.append(allProducts);
 // }
 // let productToCart = document.q
-function addToCart (e) {
-console.log(e.target.previousElementSibling);
-    console.log(e.target.parentNode.parentNode);
-    let productInfo = e.target.parentNode.parentNode;
-    // console.log(productInfo);
-    let productQtyDis = e.target.previousElementSibling;
-    let productQty = e.target.previousElementSibling.innerText;
 
-    productQty = Number(productQty);
-    productQty++
-    productQtyDis.innerText = productQty;
-    let productTitle = productInfo.querySelector(".product-title").innerText;
-    let productPrice = productInfo.querySelector(".product-price").innerText;
-    let articleTotal = productQty * productPrice;
-
-    console.log(productTitle);
-    console.log(productPrice);
-    console.log(productQty);
-
-    let article = {
-        title: productTitle, 
-        quantity: productQty,
-        price: articleTotal
-    }
-
-
-    shoppingList.push(article);
-
+function findIndexOf(article) {
+  return shoppingList.findIndex((item) => item.title == article.title); // returns the index of article or -1 if not found
 }
-    
+
+function pushToShoppingList(article, index) {
+  if (index >= 0) {
+    shoppingList[index].quantity = article.quantity;
+    shoppingList[index].price = article.price;
+  } else {
+    shoppingList.push(article);
+  }
+}
+
+function createCartArticleObject(e, productQty) {
+  let productInfo = e.target.parentNode.parentNode;
+  let productTitle = productInfo.querySelector(".product-title").innerText;
+  let productPrice = productInfo.querySelector(".product-price").innerText;
+  let articleTotal = productQty * productPrice;
+
+  let article = {
+    title: productTitle,
+    quantity: productQty,
+    price: articleTotal,
+  };
+  return article;
+}
+
+function incrementProductQty(e) {
+  let productQtyDis = e.target.previousElementSibling;
+  let productQty = e.target.previousElementSibling.innerText;
+
+  productQty = Number(productQty);
+  productQty++;
+  productQtyDis.innerText = productQty;
+
+  return productQty;
+}
+
+function decrementProductQty(e) {
+  let productQtyDis = e.target.previousElementSibling;
+  let productQty = e.target.previousElementSibling.innerText;
+
+  productQty = Number(productQty);
+  productQty--;
+  productQtyDis.innerText = productQty;
+
+  return productQty;
+}
+
+function addToCart(e) {
+  let productQty = incrementProductQty(e);
+
+  let article = createCartArticleObject(e, productQty);
+
+  let foundIndex = findIndexOf(article);
+  //   console.log(`found on index: ${foundIndex}`);
+
+  pushToShoppingList(article, foundIndex);
+  //   console.log(`shoppinglist: ${shoppingList}`);
+}
 
 // for (let index = 0; index < productQty.length; index++) {
 //     const element = productQty[index];
-    
+
 // }
 
 // }
@@ -72,28 +100,32 @@ console.log(e.target.previousElementSibling);
 let removeCartProductBtn = document.getElementsByClassName("cart-delete-btn");
 console.log(removeCartProductBtn);
 for (let index = 0; index < removeCartProductBtn.length; index++) {
-    let removeBtn = removeCartProductBtn[index];
-    removeBtn.addEventListener('click', function(event) {
-        let clickedRemoveBtn = event.target      // tar bort cart från inköpslistan
-        clickedRemoveBtn.parentElement.parentElement.remove();
-        updateCartTotal(); //funktionen updateCartTotal körs igenom när den "kallas"
-    })
+  let removeBtn = removeCartProductBtn[index];
+  removeBtn.addEventListener("click", function (event) {
+    let clickedRemoveBtn = event.target; // tar bort cart från inköpslistan
+    clickedRemoveBtn.parentElement.parentElement.remove();
+    updateCartTotal(); //funktionen updateCartTotal körs igenom när den "kallas"
+  });
 }
 // ska funktionen på rad 35 ligga här nedan? updateTotalAmount
-function updateCartTotal() { //funktionen går igenom varukorgens alla artiklar och addera för varje rad
-    let cartArticleContainer = document.getElementsByClassName("shopping-cart")[0]; //ger tillbaka en array coh vi vill bara ha en, därav [0]
-    let titleShoppingcart = cartArticleContainer.getElementsByClassName("title-shoppingcart");
-    let totalPrice = 0;
+function updateCartTotal() {
+  //funktionen går igenom varukorgens alla artiklar och addera för varje rad
+  let cartArticleContainer =
+    document.getElementsByClassName("shopping-cart")[0]; //ger tillbaka en array coh vi vill bara ha en, därav [0]
+  let titleShoppingcart =
+    cartArticleContainer.getElementsByClassName("title-shoppingcart");
+  let totalPrice = 0;
 
-    for (let index = 0; index < titleShoppingcart.length; index++) {
-        let titleShoppingcart = titleShoppingcart[i];
-        let priceElement = titleShoppingcart.getElementsByClassName("cart-product-price")[0];
-        let quantityElement = titleShoppingcart.getElementsByClassName("cart-quantity")[0];
-        
-        let price = parseFloat(priceElement.innerText.replace('€', '')) //parseFloat, för decimaler
-        let quantity = quantityElement.value //fungerar bara med input-funktionen!
-        totalPrice = tital + (price * quantity) //för att se!        
-    }
-    document.getElementsByClassName("total-price")[0].innerText = "€" + total //
+  for (let index = 0; index < titleShoppingcart.length; index++) {
+    let titleShoppingcart = titleShoppingcart[i];
+    let priceElement =
+      titleShoppingcart.getElementsByClassName("cart-product-price")[0];
+    let quantityElement =
+      titleShoppingcart.getElementsByClassName("cart-quantity")[0];
+
+    let price = parseFloat(priceElement.innerText.replace("€", "")); //parseFloat, för decimaler
+    let quantity = quantityElement.value; //fungerar bara med input-funktionen!
+    totalPrice = tital + price * quantity; //för att se!
+  }
+  document.getElementsByClassName("total-price")[0].innerText = "€" + total; //
 }
-
